2f58b50086c3e9da1a15fb7854219eb2cd7d271d
ChaitanyaAgarwal97 <chaitanyaagarwal97@gmail.com>
2023-06-12 15:19:36 +0530

    Added cors module to allow frontend to communicate

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 // app config
 app.use(express.json());
+app.use(cors());
 
 // Routes
 const routes = require("./src/routes");
@@ -12,10 +14,10 @@ const routes = require("./src/routes");
 app.use("/api", routes);
 
 app.get("*", (req, res) => {
-    return res.status(404).send("Page Not Found!!");
-})
+  return res.status(404).send("Page Not Found!!");
+});
 
 // app listener
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
-})
+  console.log(`App listening on port ${PORT}`);
+});
